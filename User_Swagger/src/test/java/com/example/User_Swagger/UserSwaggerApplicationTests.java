be14b7fdda936c8b660b0f3f794250951fe3c956
SnehaVerma14 <snehaverma142003@gmail.com>
2026-08-16 22:08:46 +0530

    chore: add initial project setup

---
+++ User_Swagger/src/test/java/com/example/User_Swagger/UserSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.User_Swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
